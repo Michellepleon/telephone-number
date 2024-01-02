59e91ae3bfcfb6388050277f5a7c415cd9f7f868
Michelle Pinar <michelle_pleon@hotmail.com>
2024-01-01 20:18:04 -0500

    init project with basic code

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+inputs: ("9238372", ["minou", "michelle", "carl", ...]
+output: ["minou", "michelle"]
+description: find the words that can be associated with certain telephone numbers
+1: nothing
+2: abc
+3: def
+4: ghi
+5: jkl
+6: mno
+7: pqrs
+8: tuv
+9: wxyz
+0: nothing
+"""
+def find_words(number_as_string, array_of_words_as_string):
+
+
+
+result =  find_words("120746582", ["allo", "bye", "jesus", "madonna", "croquettes"])
